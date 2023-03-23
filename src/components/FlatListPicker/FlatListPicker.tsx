879bb9d287d545599c8ca664fbaad5945cf372ca
Monica Rivera <mondev811@gmail.com>
2023-03-23 00:57:18 -0700

    Add boilerplate

--- src/components/FlatListPicker/FlatListPicker.tsx
+++ src/components/FlatListPicker/FlatListPicker.tsx
@@ -1,6 +1,16 @@
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {
+  Alert,
+  Dimensions,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import data from './data';
 
 const ICON_SIZE = 42;
 const ITEM_HEIGHT = ICON_SIZE * 2;
@@ -26,20 +36,106 @@ const Item = React.memo(
         ) : (
           <View />
         )}
+        <Icon icon={icon} color={color} />
       </View>
     );
   },
 );
-export const FlatListPicker = () => {
+
+const ConnectWithText = React.memo(() => {
   return (
-    <View>
-      <SimpleLineIcons name="user-female" color="red" size={42} />
-      <Text>FlatListPicker</Text>
+    <View
+      style={{
+        position: 'absolute',
+        top: height / 2 - ITEM_HEIGHT * 2,
+        width: width * 0.7,
+        paddingHorizontal: 14,
+      }}>
+      <Text
+        style={{
+          color: colors.yellow,
+          fontSize: 52,
+          fontWeight: '700',
+          lineHeight: 52,
+        }}>
+        Connect with...
+      </Text>
+    </View>
+  );
+});
+
+const ConnectButton = React.memo((props: {onPress: () => void}) => {
+  const {onPress} = props;
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        top: height / 2 + ITEM_HEIGHT / 2,
+        paddingHorizontal: 14,
+      }}>
+      <View
+        style={{
+          height: ITEM_HEIGHT * 3,
+          width: 2,
+          backgroundColor: colors.yellow,
+        }}
+      />
+      <TouchableOpacity
+        onPress={onPress}
+        style={{
+          paddingVertical: 10,
+          paddingHorizontal: 12,
+          backgroundColor: colors.yellow,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        activeOpacity={0.8}>
+        <Text style={{fontSize: 32, fontWeight: '800', color: colors.dark}}>
+          Done!
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+});
+
+export const FlatListPicker = () => {
+  const [index, setIndex] = React.useState(0);
+  const onConnectPress = React.useCallback(() => {
+    Alert.alert('Connect with: ', data[index].name.toUpperCase());
+  }, [index]);
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <ConnectWithText />
+      <Item {...data[0]} showText color={colors.yellow} />
+      <ConnectButton onPress={onConnectPress} />
+      <Item />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  itemWrapper: {},
-  itemText: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: colors.dark,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  itemWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: ITEM_HEIGHT,
+  },
+  itemText: {
+    fontSize: 26,
+    fontWeight: '800',
+    textTransform: 'capitalize',
+  },
 });
